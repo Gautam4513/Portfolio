966ba73d9a0f30199a1ed8f246f0d40a9f68eaee
Gautam Mandaviya <gautammandaviya21@gmail.com>
2025-03-08 13:47:39 +0530

    add readme.md

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -43,7 +43,7 @@ const Hero = () => {
 
     return (
         <section className='min-h-screen w-full flex flex-col relative' id='home'>
-            <div className='w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3'>
+            <div className='w-full mx-auto flex  flex-col sm:mt-36 mt-20 c-space gap-3'>
                 <p className='sm:text-3xl text-2xl font-medium text-white text-center font-roboto '>Hi, I am Gautam <span className='waving-hand'>👋</span></p>
                 <p className='hero_tag text-gray_gradient'>
                     Turning Imagination Into Reality
